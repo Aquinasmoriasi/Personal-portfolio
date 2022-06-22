a6434889d32e363c7b9e720739ee04d99b3ef072
unknown <aquinasmoriasi@gmail.com>
2022-06-22 22:54:08 +0300

    Add event listener to the close button

--- script.js
+++ script.js
@@ -53,74 +53,9 @@ navLink3.addEventListener('click', () => {
 
 
 // //popups section
+  let close = document.querySelector('#close-btn');
+  let project1 = document.querySelector('#pj1');
 
-// // const projectOneMobile = {
-// //   highlight: 'img',
-// //   header: 'h2',
-// //   otherProjects: '.highlights',
-// //   projectDescription: '.project1-description-p',
-// //   techDetails: '.project1-tech-details',
-// //   getResume: '.resume'
-// // }
-
-// let project = document.querySelectorAll('.resume');
-// let body = document.querySelectorAll('body');
-// project.addEventListener('click', () => {
-//   document.createElement('div');
-//   div.classList.add('popup');
-//   const popup = document.querySelector('.popup');
-//   body.appendChild('popup');
-//   popup.style.width = '70vw';
-//   popup.style.height = '70vh';
-//   div.style.backgroundColor = 'red';
-// })
-
-// {/* <section class="project1">
-//            <img src="./images/project1.jpg" alt="project1-snapshot">
-//            <h2 id="pj1-h2">Tonic</h2>
-//            <h2 id="pj1-h2-alt">Facebook 360</h2>
-//            <p class="highlights">
-//              <span class="canopy4">CANOPY</span>
-//              <span class="canopy-alt">FACEBOOK</span>
-//                 <span>
-//                 <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-//                 <circle cx="4" cy="4" r="4" fill="#C1C7D0"/>
-//                 </svg>
-//                 </span>
-//                 <span class="not-canopy4">Back End Dev</span>
-//                 <span class="not-canopy-alt">Full Stack Dev</span>
-//                 <span>
-//                 <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-//                 <circle cx="4" cy="4" r="4" fill="#C1C7D0"/>
-//                 </svg>
-//                 </span>
-//                 <span class="not-canopy4">2015</span>
-//                 <span class="not-canopy-alt">2015</span>
-           
-//             <div class="project1-description">
-//                 <p class="project1-description-p">A daily selection of privately personalized reads; no accounts or sign-ups required.</p>
-//           <p class="project1-description-alt-p">
-//             Exploring the future of media in Facebook's first Virtual Reality app;
-//             a place to discover and enjoy 360 photos and videos on Gear VR.
-//           </p>
-//             </div>
-//          <div class="project1-tech-details">
-//             <ul class="lang-buttons">
-//                 <li>
-//                     <button type="button">html</button>
-//                 </li>
-//                 <li>
-//                     <button class="pj4-alt-lang-button" type="button">Ruby on rails</button>
-//                 </li>
-//                 <li>
-//                     <button type="button">css</button>
-//                  </li>
-//                 <li>
-//                     <button type="button">javaScript</button>
-//                 </li>
-//               </ul>
-//          </div>   
-//          <div class="resume">
-//             <a href="#">See Project</a> 
-//          </div>    
-//        </section> */}
+  close.addEventListener('click', () => {
+    project1.style.display = 'none';
+  })
